b88b5495c64b17a0628e0aca465d368d8d9f2e99
ashutosh sahu <ppaskumar@gmail.com>
2024-08-31 07:39:58 +0530

    live chat added

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,23 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState();
+
+  const ChatMessages = useSelector((store) => store.chat.messages);
+  const dispatch = useDispatch();
   useEffect(() => {
     const i = setInterval(() => {
-      console.log("Hello Sahu");
-    }, 2000);
+      dispatch(
+        addMessage({
+          name: generateRandomName(),
+          message: makeRandomMessage(20),
+        })
+      );
+    }, 1000);
 
     return () => clearInterval(i);
   }, []);
 
   return (
-    <div className="w-full  h-[400px] m-2 p-2 border border-black bg-slate-100 rounded-lg">
-      <ChatMessage name="ashutosh sahu" message="This is namaste" />
-      <ChatMessage name="ashutosh sahu" message="This is namaste" />
-      <ChatMessage name="ashutosh sahu" message="This is namaste" />
-      <ChatMessage name="ashutosh sahu" message="This is namaste" />
-      <ChatMessage name="ashutosh sahu" message="This is namaste" />
-      <ChatMessage name="ashutosh sahu" message="This is namaste" />
+    <div>
+      <div className="w-full  h-[400px] m-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        {ChatMessages.map((c) => (
+          <ChatMessage name={c.name} message={c.message} />
+        ))}
+      </div>
+      <form
+        className="w-full p-2 ml-2 border border-black"
+        onSubmit={(e) => {
+          console.log("hello", liveMessage);
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Ashutosh Shau",
+              message: liveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="px-2  w-78 border border-black"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className="px-2 mx-10 bg-green-800 rounded-lg">Send</button>
+      </form>
     </div>
   );
 };
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,7 @@
 const GOOGLE_API_KEY = "AIzaSyAi8nAdpZrtty6y1YZ4FoUWXqSfs-eER2M";
 
+export const OFFSET_LIVE_CHAT = 30;
+
 export const YOUTUBE_VIDEOES_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
